71d5067cc3ed2e38dd1160d9d4395978fda3deb0
Sung <k458850@gmail.com>
2017-09-15 19:37:13 +0800

    version 2

--- js/index.js
+++ js/index.js
@@ -16,11 +16,25 @@ $(function () {
         //    uvData = data;
         //});
 
-        $.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314", function (data) {
-            console.dir(data);
-            uvData = data;
+        //$.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314", function (data) {
+        //    console.dir(data);
+        //    uvData = data;
+        //});
+        $.ajax({
+            type: "get",
+            async: true,//async設定false會變成同步請求 要完成ajax後才會繼續執行
+            url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314",
+            dataType: "jsonp",
+            jsonpCallback: "?", //callback函式(jsonp的callback函式，預設是callback)
+            success: function (json) {
+                //jQuery會自動將結果傳入(如果有設定callback函式的話，兩者都會執行)
+                alert('success');
+                console.log(json);
+            },
+            error: function () {
+                alert('fail');
+            }
         });
-
     });
 
 })
