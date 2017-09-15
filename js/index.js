22a6d0167d3170b0f6b604782906a9e0f86d0b05
Sung <k458850@gmail.com>
2017-09-15 21:21:15 +0800

    API完成

--- js/index.js
+++ js/index.js
@@ -1,40 +1,33 @@
-var weatherData = "",uvData="";
+var weatherData = "", uvData = "";
+//http://api.openweathermap.org/img/w/.png
 $(function () {
     navigator.geolocation.getCurrentPosition(function (location) {
-        console.log(location.coords.latitude);
-        console.log(location.coords.longitude);
+        //console.log(location.coords.latitude);
+        //console.log(location.coords.longitude);
 
         ///*weather*/
-        //$.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude +"&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
-        //    console.dir(data);
-        //    weatherData = data;
-        //});
+        $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude +"&units=metric&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+            console.dir(data);
+            weatherData = data;
+            console.log(weatherData.main.temp);
+            console.log(weatherData.weather[0].description);
+            console.log(weatherData.weather[0].icon);
 
-        ///*UV*/
-        //$.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
-        //    console.dir(data);
-        //    uvData = data;
-        //});
-
-        //$.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314", function (data) {
-        //    console.dir(data);
-        //    uvData = data;
-        //});
-        $.ajax({
-            type: "get",
-            async: true,//async設定false會變成同步請求 要完成ajax後才會繼續執行
-            url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314",
-            dataType: "jsonp",
-            jsonpCallback: "?", //callback函式(jsonp的callback函式，預設是callback)
-            success: function (json) {
-                //jQuery會自動將結果傳入(如果有設定callback函式的話，兩者都會執行)
-                alert('success');
-                console.log(json);
-            },
-            error: function () {
-                alert('fail');
-            }
+            $("#address").html("<h1>" + weatherData.name + "</h1>");
+            $("#iconImg").html("<img src='http://api.openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png' />");
+            $("#description").html("<h2>" + weatherData.weather[0].description + "</h2>");
         });
+
+        /*UV*/
+        $.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+            console.dir(data);
+            uvData = data;
+            console.log(uvData[0].value);
+            $("#uvValue").html("<h2>UV: " + uvData[0].value + "</h2>");
+        });
+
+        
+       
     });
 
 })
