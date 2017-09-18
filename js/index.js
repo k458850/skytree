bc22ff68f9844757d0781c3db069eba74fc04e1f
k458850 <k458850@gmail.com>
2017-09-18 12:22:27 +0800

    例外修改

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 var weatherData = "", uvData = "";
 //http://api.openweathermap.org/img/w/.png
 $(function () {
-    navigator.geolocation.getCurrentPosition(function (location) {
-        //console.log(location.coords.latitude);
-        //console.log(location.coords.longitude);
 
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(getInformation,errorCallback);
+    } else {
+        alert('您的瀏覽器不支援定位功能');
+        var gpsData={"coords":{"latitude":'24.1372291',"longitude":'120.6809453'}}
+        getInformation(gpsData);
+    }
+
+    function getInformation(position){
         ///*weather*/
-        $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude +"&units=metric&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+        console.dir(position);
+        $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&units=metric&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
             console.dir(data);
             weatherData = data;
             console.log(weatherData.main.temp);
@@ -19,16 +27,24 @@ $(function () {
         });
 
         /*UV*/
-        $.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+        $.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
             console.dir(data);
             uvData = data;
             console.log(uvData[0].value);
             $("#uvValue").html("<h2>UV: " + uvData[0].value + "</h2>");
         });
+    }
 
-        
-       
-    });
+    function errorCallback(error) {
+      var errorTypes={
+            0:"不明原因錯誤",
+            1:"使用者拒絕提供位置資訊",
+            2:"無法取得位置資訊",
+            3:"位置查詢逾時"
+            };
+      var gpsData={"coords":{"latitude":'24.1372291',"longitude":'120.6809453'}}
+        getInformation(gpsData);
+    }
 
 })
 
